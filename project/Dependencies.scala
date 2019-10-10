255052d23bc8f4fb44cbdd45bacced75ec1c80cf
Eric Loots <eric.loots@gmail.com>
2019-10-10 14:17:14 +0200

    Remove unused dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,6 @@ object Version {
   val akkaVer         = "2.5.25"
   val logbackVer      = "1.2.3"
   val scalaVer        = "2.12.10"
-  val scalaParsersVer = "1.0.6"
   val scalaTestVer    = "3.0.8"
 
 }
@@ -14,7 +13,6 @@ object Dependencies {
     "com.typesafe.akka"       %% "akka-actor"                 % Version.akkaVer,
     "com.typesafe.akka"       %% "akka-slf4j"                 % Version.akkaVer,
     "ch.qos.logback"           % "logback-classic"            % Version.logbackVer,
-    "org.scala-lang.modules"  %% "scala-parser-combinators"   % Version.scalaParsersVer,
     "com.typesafe.akka"       %% "akka-testkit"               % Version.akkaVer % "test",
     "org.scalatest"           %% "scalatest"                  % Version.scalaTestVer % "test"
   )
